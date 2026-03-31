271263f2358cc0e59f48174360e979309fe77eac
pvoleak55 <pvoleak55@gmail.com>
2026-03-31 11:30:19 +0700

    change port

--- src/main/java/com/example/crudapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/crudapi/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
